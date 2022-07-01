774f322c566b3203151b1bf951320015bd1bf998
GideonHale <gideonghale@gmail.com>
2022-07-01 06:38:41 -0600

    Compiles now

--- Lab2_Navigation/Navigation.cpp
+++ Lab2_Navigation/Navigation.cpp
@@ -10,10 +10,13 @@ Navigation::~Navigation() {
 
 bool Navigation::readNetwork(string _mapName) {
     cout << "** in Navigation readNetwork() **" << endl;
+    return 0;
 }
 
 queue<int> Navigation::computeShortestPath(int _startInd, int _endInd) {
     cout << "** in Navigation computeShortestPath() **" << endl;
+    queue<int> paths;
+    return paths;
 }
 
 void Navigation::printPath(queue<int> _path) {
